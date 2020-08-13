b7c7be74375584c080277c8e6a1578999a7a989b
Jing Qian <qianjing2020@users.noreply.github.com>
2020-08-13 18:32:59 -0400

    initial commit

--- adv.py
+++ adv.py
@@ -10,11 +10,11 @@ world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
-# map_file = "maps/test_line.txt"
+map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-map_file = "maps/main_maze.txt"
+#map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -27,8 +27,57 @@ player = Player(world.starting_room)
 
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
+""" move player around to traverse all graph
+All rooms need to be covered.
+Record total moves len(traversal_path) """
 traversal_path = []
 
+def get_neigbors(room):
+    # get ajacent rooms accessible from given room_id in world
+    neighbors = []
+    # check all four directions    
+    for direction in ['n', 's', 'w', 'e']:
+        next_room = room.get_room_in_direction('direction')
+        if next_room is not None:
+            neighbors.append(next_room)
+
+    return neighbors
+
+class Stack():
+    def __init__(self):
+        self.stack = []
+    def push(self, value):
+        self.stack.append(value)
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+    def size(self):
+        return len(self.stack)
+
+def dft(curr_room, visited_rooms):
+    # Given current room and visited_rooms set, 
+    # use DFT to store each step along the path
+    s = Stack()
+    s.push(curr_room)
+    path = []
+    while s.size() > 0:
+        v = s.pop()
+        # visited or not, we want path to record this vertex
+        path.append(v)
+        if v not in visited_rooms:
+            visited_rooms.append(v)
+            
+            for neighbor in get_neigbors(v):
+                s.push(neighbor)
+
+    return path
+
+
+
+
+breakpoint()
 
 
 # TRAVERSAL TEST - DO NOT MODIFY
@@ -42,10 +91,10 @@ for move in traversal_path:
 
 if len(visited_rooms) == len(room_graph):
     print(f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
+
 else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
-
 
 
 #######
