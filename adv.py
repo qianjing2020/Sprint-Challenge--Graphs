5aac6c895d6eff4a00708bb010db45e296219d49
Jing Qian <qianjing2020@users.noreply.github.com>
2020-08-14 13:47:08 -0400

    middle"

--- adv.py
+++ adv.py
@@ -10,8 +10,8 @@ world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
-map_file = "maps/test_line.txt"
-# map_file = "maps/test_cross.txt"
+#map_file = "maps/test_line.txt"
+map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
 #map_file = "maps/main_maze.txt"
@@ -32,15 +32,39 @@ All rooms need to be covered.
 Record total moves len(traversal_path) """
 traversal_path = []
 
+
+def get_room_graph(world):
+    """ 
+    Construct room graphs from the world object. 
+    vertices are: 
+    {
+        0: {'n': '?', 's': 5, 'w': '?', 'e': '?'},
+        5: {'n': 0, 's': '?', 'e': '?'}
+    }
+    """      
+    room_graph = {}
+    rooms = world.rooms
+    for n in rooms:
+        r = rooms[n]
+        key = r.id
+        # initiate a dictionary with direction as key, room in direction as value
+        value = {'n': '?', 's': '?', 'w': '?', 'e': '?'}
+        
+        for D in r.get_exits():
+            value[D] = r.get_room_in_direction(D).id
+        
+        room_graph[key] = value
+    return room_graph
+    
+
 def get_neigbors(room):
     # get ajacent rooms accessible from given room_id in world
     neighbors = []
     # check all four directions    
     for direction in ['n', 's', 'w', 'e']:
-        next_room = room.get_room_in_direction('direction')
+        next_room = room.get_room_in_direction(direction)
         if next_room is not None:
-            neighbors.append(next_room)
-
+            neighbors.append(next_room)    
     return neighbors
 
 class Stack():
@@ -56,30 +80,45 @@ class Stack():
     def size(self):
         return len(self.stack)
 
-def dft(curr_room, visited_rooms):
+def dft(curr_room, visited_rooms=None, path=None):
     # Given current room and visited_rooms set, 
     # use DFT to store each step along the path
-    s = Stack()
-    s.push(curr_room)
-    path = []
-    while s.size() > 0:
-        v = s.pop()
-        # visited or not, we want path to record this vertex
-        path.append(v)
-        if v not in visited_rooms:
-            visited_rooms.append(v)
-            
-            for neighbor in get_neigbors(v):
-                s.push(neighbor)
-
+    
+    if visited_rooms  == None:
+        visited_rooms = set()
+    if path == None:
+        path = []
+    
+    visited_rooms.add(curr_room)
+    path += [curr_room]
+    
+    for r in get_neigbors(curr_room):
+        if r not in visited_rooms:
+            dft(r, visited_rooms, path)
+    
     return path
 
 
+def get_direction_from_path(path):
+    # get the id of the room in path
+    ids = [item.id for item in path]
+    # initialize edges
+    edges =  ['?' for item in range(len(ids)-1)]
+    # loop through ids
+    for n, id in enumerate(ids):
+        for D in ['n', 's', 'e', 'w']:
+            if path[id].get_room_in_direction(D) == path[ids[n+1]]:
+                edges[n] = D
+    return edges
 
 
+current_room = world.starting_room
+path = dft(current_room)
+path_id = [item.id for item in path]
+print('LLLLLLLLLL')
+
+print(get_room_graph(world))
 breakpoint()
-
-
 # TRAVERSAL TEST - DO NOT MODIFY
 visited_rooms = set()
 player.current_room = world.starting_room
