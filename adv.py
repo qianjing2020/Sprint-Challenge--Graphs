93d3e1f6b32cf4448349df85e011e45974d369fb
Jing Qian <qianjing2020@users.noreply.github.com>
2020-08-14 17:06:26 -0400

    final:

--- adv.py
+++ adv.py
@@ -5,16 +5,28 @@ from world import World
 import random
 from ast import literal_eval
 
+class Stack():
+    def __init__(self):
+        self.stack = []
+    def push(self, value):
+        self.stack.append(value)
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+    def size(self):
+        return len(self.stack)
+
 # Load world
 world = World()
 
-
 # You may uncomment the smaller graphs for development and testing purposes.
 #map_file = "maps/test_line.txt"
-map_file = "maps/test_cross.txt"
-# map_file = "maps/test_loop.txt"
+# map_file = "maps/test_cross.txt"
+#map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-#map_file = "maps/main_maze.txt"
+map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -30,95 +42,102 @@ player = Player(world.starting_room)
 """ move player around to traverse all graph
 All rooms need to be covered.
 Record total moves len(traversal_path) """
+
 traversal_path = []
 
+class RoomGraph():
+    def __init__(self, world):        
+        """ 
+        Construct room graphs from the world object. 
+        vertices are: 
+        {   0: {'n': '?', 's': 5, 'w': '?', 'e': '?'},
+            5: {'n': 0, 's': '?', 'e': '?'}
+        }
+        """ 
+        rooms = world.rooms
+        self.size = len(rooms)
+        self.graph = {}
+        for n in rooms:
+            r = rooms[n]            
+            # initiate key, value for each item in graph
+            key = r.id
+            value = {'n': None, 's': None, 'w': None, 'e': None}
+            # updated wherever there is a valid exit 
+            for D in r.get_exits():
+                value[D] = r.get_room_in_direction(D).id
+            
+            self.graph[key] = value           
+    
+    def get_neigbors(self, room_id):
+        # get ajacent rooms accessible to room
+        about = self.graph[room_id]
+        neighbors = {key: value for key, value in about.items() if value is not None}    
+        return neighbors
 
-def get_room_graph(world):
-    """ 
-    Construct room graphs from the world object. 
-    vertices are: 
-    {
-        0: {'n': '?', 's': 5, 'w': '?', 'e': '?'},
-        5: {'n': 0, 's': '?', 'e': '?'}
-    }
-    """      
-    room_graph = {}
-    rooms = world.rooms
-    for n in rooms:
-        r = rooms[n]
-        key = r.id
-        # initiate a dictionary with direction as key, room in direction as value
-        value = {'n': '?', 's': '?', 'w': '?', 'e': '?'}
+    def dft(self, curr_room_id, visited_rooms=None, path=None):
+        # Given current room and visited_rooms set, 
+        # use DFT to store each step along the path        
+        if visited_rooms  == None:
+            visited_rooms = set()
+        if path == None:
+            path = []        
+        visited_rooms.add(curr_room_id)
+        # path += [curr_room_id] 
+
+        # Get (Direction, Room id) for each neigbor            
+        for D, R in self.get_neigbors(curr_room_id).items():
+            if R not in visited_rooms:
+                path.append(D)
+                self.dft(R, visited_rooms, path)
         
-        for D in r.get_exits():
-            value[D] = r.get_room_in_direction(D).id
-        
-        room_graph[key] = value
-    return room_graph
-    
+        return path
+   
+    def player_wonder_about(self, starting_room):
+        # create a player
+        player = Player(starting_room)
+        # visited nodes storage
+        visited = set()
+        # upfront nodes storage
+        s = Stack()
+        # a map for back track once reach a node whose neighbors have been exhausted
+        backtrack = {'s':'n','n':'s','w':'e','e':'w'}
 
-def get_neigbors(room):
-    # get ajacent rooms accessible from given room_id in world
-    neighbors = []
-    # check all four directions    
-    for direction in ['n', 's', 'w', 'e']:
-        next_room = room.get_room_in_direction(direction)
-        if next_room is not None:
-            neighbors.append(next_room)    
-    return neighbors
+        # as long as there are room not visited in the graph, do this:
+        while len(visited) < len(self.graph):
+            # find valid exit 
+            exits = player.current_room.get_exits()
+            # creat a branches list for current valid directions to go
+            branches = []
+            # go through each direction
+            for D in exits:
+                if (D != None) and (player.current_room.get_room_in_direction(D) not in visited):
+                    branches.append(D)
+            # add curr to visited 
+            visited.add(player.current_room)
+            
+            # if there are more than 1 direction to go 
+            if len(branches) > 0 :
+                # generate a random number for direction to go
+                rand = random.randint(0, len(branches)-1)
+                direction = branches[rand]
+                # push direction to stack and go there
+                s.push(direction)
+                player.travel(direction)
+                traversal_path.append(direction)
+            else:
+                previous_D = s.pop() 
+                # new direction for player to go back to previous room
+                reverse_D = backtrack[previous_D]
+                # player goes back
+                player.travel(reverse_D)
+                traversal_path.append(reverse_D)
+        return traversal_path
 
-class Stack():
-    def __init__(self):
-        self.stack = []
-    def push(self, value):
-        self.stack.append(value)
-    def pop(self):
-        if self.size() > 0:
-            return self.stack.pop()
-        else:
-            return None
-    def size(self):
-        return len(self.stack)
+x = world.starting_room
+g = RoomGraph(world)
+print(f"neighbors:{g.get_neigbors(x.id)}")
+traversal_path = g.player_wonder_about(x)
 
-def dft(curr_room, visited_rooms=None, path=None):
-    # Given current room and visited_rooms set, 
-    # use DFT to store each step along the path
-    
-    if visited_rooms  == None:
-        visited_rooms = set()
-    if path == None:
-        path = []
-    
-    visited_rooms.add(curr_room)
-    path += [curr_room]
-    
-    for r in get_neigbors(curr_room):
-        if r not in visited_rooms:
-            dft(r, visited_rooms, path)
-    
-    return path
-
-
-def get_direction_from_path(path):
-    # get the id of the room in path
-    ids = [item.id for item in path]
-    # initialize edges
-    edges =  ['?' for item in range(len(ids)-1)]
-    # loop through ids
-    for n, id in enumerate(ids):
-        for D in ['n', 's', 'e', 'w']:
-            if path[id].get_room_in_direction(D) == path[ids[n+1]]:
-                edges[n] = D
-    return edges
-
-
-current_room = world.starting_room
-path = dft(current_room)
-path_id = [item.id for item in path]
-print('LLLLLLLLLL')
-
-print(get_room_graph(world))
-breakpoint()
 # TRAVERSAL TEST - DO NOT MODIFY
 visited_rooms = set()
 player.current_room = world.starting_room
@@ -139,12 +158,12 @@ else:
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
